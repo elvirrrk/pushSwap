a15c2c34cc53434242dda9800e94346e3ec51e29
Elvira Fatin <efatin@f6r11s5.paris.42.school>
2026-07-10 00:04:53 +0200

    parsing

--- parsing.c
+++ parsing.c
@@ -1,51 +1,39 @@
 #include "push_swap.c"
 
-int	is_bench(char **argc)
+int	is_bench(char *argc, t_data data)
 {
 	char bench[7];
 
 	bench = "--bench";
 
-	if (!ft_strcmp(argc[1], bench) && !ft_strcmp(argc[2], bench))
-		return (err);
-	if ((!ft_strcmp(argc[1], bench) && ft_strcmp(argc[2], bench)))
-	{
-		data->bench = 1;
-		return (1);
-	}
-	if ((ft_strcmp(argc[1], bench) && !ft_strcmp(argc[2], bench)))
-	{
-		data->bench = 1;
-		return (1);
-	}
-	return (0);
+	if (ft_strcmp(argc, bench) != 0)
+		return (0);
+	if (data->bench)
+		error();
+	data->bench = 1;
+	return (1);
 }
 
-int	is_strategy(char **argc)
+int	is_strategy(char *argc, t_data data)
 {
-	if (what_is_startegy(argc[i]))
-	{
-		data->strategy = chose_strategy(argc[i]);
-		i++;
-	}
+	if (what_is_startegy(argc))
+		return (0);
+	if (ft_strcmp(data->strategy, argc))
+		data->strategy = chose_strategy(argc);
 }
 
-int parsing(int argv, char **argc)
+int parsing(int argv, char **argc, t_data data)
 {
 	int	i;
 
 	i = 1;
 
-	if (is_strategy || )
+	while (i < 3)
 	{
-		data->strategy = chose_strategy(argc[i]);
-		i++;
-	}
-
-	if (what_is_startegy(argc[i]))
-	{
-		data->strategy = chose_strategy(argc[i]);
-		i++;
+		if (is_bench(argc[i], data))
+			i++;
+		if (is_strategy(argc[i], data))
+			i++;
 	}
 
 	// check input for arguments that are not integers,
